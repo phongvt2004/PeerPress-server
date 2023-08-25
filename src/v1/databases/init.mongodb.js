9fccfedc8cdc9a9741ab9c07ea56315d8c252645
phongvt2004 <ntp11892vt@gmail.com>
2023-08-26 01:35:28 +0700

    update slug

--- src/v1/databases/init.mongodb.js
+++ src/v1/databases/init.mongodb.js
@@ -14,6 +14,6 @@ mongoose.set('debug', { color: false })
 // get mongodb-shell friendly output (ISODate)
 mongoose.set('debug', { shell: true })
 
-mongoose.plugin(require('mongoose-slug-generator'));
+mongoose.plugin(require('mongoose-slug-updater'));
 
 module.exports = mongoose;
