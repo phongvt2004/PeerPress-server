e96e64c8e90ce410d5d3867e69a33bb4516d2e21
phongvt2004 <ntp11892vt@gmail.com>
2023-08-25 16:41:21 +0700

    update route

--- src/v1/routes/press.router.js
+++ src/v1/routes/press.router.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const PressController = require('../controllers/press.controller')
 
 
+router.post('/create', PressController.create)
+
 router.get('/test', (req, res) => {
     res.json("data")
 })
