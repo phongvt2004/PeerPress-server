723d1a11642fd49b8f8876af36c9fb85ea93f360
phongvt2004 <ntp11892vt@gmail.com>
2023-10-09 04:39:07 +0700

    update consult and email

--- src/v1/routes/email.router.js
+++ src/v1/routes/email.router.js
@@ -4,7 +4,7 @@ const EmailController = require('../controllers/email.controller')
 const {verifyToken} = require('../middlewares/jwt.middleware')
 
 
-router.post('/add', EmailController.create)
+router.post('/add', EmailController.add)
 
 router.get('/test', (req, res) => {
     res.json("data")
