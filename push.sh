cef9b4e17e305166237d8cfbf3c1921e95669ef4
phongvt2004 <ntp11892vt@gmail.com>
2023-10-12 14:59:46 +0700

    update

--- push.sh
+++ push.sh
@@ -1,2 +1,3 @@
 echo 'PeerPress2023' | ssh root@$116.118.51.13 'bash -s'< ./deploy.sh
 # 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
+# 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
